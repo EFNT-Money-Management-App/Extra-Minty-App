4323c09a38c4112f11155d398950df96895da90b
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2023-01-07 15:14:43 -0500

    added back end

---
+++ extra-minty-back-end/src/test/java/com/minty/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.minty;
+
+import com.minty.MintyMicroServiceApp;
+import com.minty.config.AsyncSyncConfiguration;
+import com.minty.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { MintyMicroServiceApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
