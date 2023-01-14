9c21634991f3788fa134b291b885c12440830013
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2023-01-14 17:33:52 -0500

    finished with the budget styling

--- extra-minty-back-end/src/main/webapp/app/entities/budget/budget-update.tsx
+++ extra-minty-back-end/src/main/webapp/app/entities/budget/budget-update.tsx
@@ -89,100 +89,97 @@ export const BudgetUpdate = (props) => {
 
   return (
     <div>
-      <button onClick={handleShow}>
-        {isNew ? "Add Budget" : "Update Budget"}
-      </button>
-      
+      <Button style={{ background: '#00c314', border: '#00c314'}} onClick={handleShow}>
+        {isNew ? 'Add Budget' : 'Update Budget'}
+      </Button>
+
       <Modal show={show} onHide={handleClose}>
-
-
-      <Row className="justify-content-center">
-        <Col md="8">
-          <h2 id="extraMintyApp.budget.home.createOrEditLabel" data-cy="BudgetCreateUpdateHeading">
-            <Translate contentKey="extraMintyApp.budget.home.createOrEditLabel">Create or edit a Budget</Translate>
-          </h2>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col md="8">
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <ValidatedForm defaultValues={defaultValues()} onSubmit={saveEntity}>
-              {!isNew ? (
+        <Row className="justify-content-center">
+          <Col md="8">
+            <h2 id="extraMintyApp.budget.home.createOrEditLabel" data-cy="BudgetCreateUpdateHeading">
+              <Translate contentKey="extraMintyApp.budget.home.createOrEditLabel">Create or edit a Budget</Translate>
+            </h2>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col md="8">
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              <ValidatedForm defaultValues={defaultValues()} onSubmit={saveEntity}>
+                {!isNew ? (
+                  <ValidatedField
+                    name="id"
+                    required
+                    readOnly
+                    id="budget-id"
+                    label={translate('global.field.id')}
+                    validate={{ required: true }}
+                  />
+                ) : null}
                 <ValidatedField
-                  name="id"
-                  required
-                  readOnly
-                  id="budget-id"
-                  label={translate('global.field.id')}
-                  validate={{ required: true }}
+                  label={translate('extraMintyApp.budget.spendingLimit')}
+                  id="budget-spendingLimit"
+                  name="spendingLimit"
+                  data-cy="spendingLimit"
+                  type="text"
                 />
-              ) : null}
-              <ValidatedField
-                label={translate('extraMintyApp.budget.spendingLimit')}
-                id="budget-spendingLimit"
-                name="spendingLimit"
-                data-cy="spendingLimit"
-                type="text"
-              />
-              <ValidatedField
-                label={translate('extraMintyApp.budget.currentSpending')}
-                id="budget-currentSpending"
-                name="currentSpending"
-                data-cy="currentSpending"
-                type="text"
-              />
-              <ValidatedField label={translate('extraMintyApp.budget.name')} id="budget-name" name="name" data-cy="name" type="text" />
-              <ValidatedField
-                label={translate('extraMintyApp.budget.monthOfDate')}
-                id="budget-monthOfDate"
-                name="monthOfDate"
-                data-cy="monthOfDate"
-                type="select"
-              >
-                {monthValues.map(month => (
-                  <option value={month} key={month}>
-                    {translate('extraMintyApp.Month.' + month)}
-                  </option>
-                ))}
-              </ValidatedField>
-              <ValidatedField
-                label={translate('extraMintyApp.budget.budgetYear')}
-                id="budget-budgetYear"
-                name="budgetYear"
-                data-cy="budgetYear"
-                type="text"
-              />
-              <ValidatedField id="budget-user" name="user" data-cy="user" label={translate('extraMintyApp.budget.user')} type="select">
-                {isAdmin ? <option value="" key="0" /> : <div>{account.login}</div>}
-                {isAdmin
-                  ? users.map(otherEntity => (
+                <ValidatedField
+                  label={translate('extraMintyApp.budget.currentSpending')}
+                  id="budget-currentSpending"
+                  name="currentSpending"
+                  data-cy="currentSpending"
+                  type="text"
+                />
+                <ValidatedField label={translate('extraMintyApp.budget.name')} id="budget-name" name="name" data-cy="name" type="text" />
+                <ValidatedField
+                  label={translate('extraMintyApp.budget.monthOfDate')}
+                  id="budget-monthOfDate"
+                  name="monthOfDate"
+                  data-cy="monthOfDate"
+                  type="select"
+                >
+                  {monthValues.map(month => (
+                    <option value={month} key={month}>
+                      {translate('extraMintyApp.Month.' + month)}
+                    </option>
+                  ))}
+                </ValidatedField>
+                <ValidatedField
+                  label={translate('extraMintyApp.budget.budgetYear')}
+                  id="budget-budgetYear"
+                  name="budgetYear"
+                  data-cy="budgetYear"
+                  type="text"
+                />
+                <ValidatedField id="budget-user" name="user" data-cy="user" label={translate('extraMintyApp.budget.user')} type="select">
+                  {isAdmin ? <option value="" key="0" /> : <div>{account.login}</div>}
+                  {isAdmin ? (
+                    users.map(otherEntity => (
                       <option value={otherEntity.id} key={otherEntity.id}>
                         {otherEntity.login}
                       </option>
                     ))
-                  : <option>
-                      {account.login}
-                    </option>}
-              </ValidatedField>
-              {/* <Button tag={Link} id="cancel-save" data-cy="entityCreateCancelButton" to="/userbudget" replace color="info">
+                  ) : (
+                    <option>{account.login}</option>
+                  )}
+                </ValidatedField>
+                {/* <Button tag={Link} id="cancel-save" data-cy="entityCreateCancelButton" to="/userbudget" replace color="info">
                 &nbsp;
                 <span className="d-none d-md-inline">
                   <Translate contentKey="entity.action.cancel">Cancel</Translate>
                 </span>
               </Button> */}
-              &nbsp;
-              <Button color="primary" id="save-entity" data-cy="entityCreateSaveButton" type="submit" disabled={updating}>
-                <FontAwesomeIcon icon="save" />
                 &nbsp;
-                <Translate contentKey="entity.action.save">Save</Translate>
-              </Button>
-            </ValidatedForm>
-          )}
-        </Col>
-      </Row>
-
+                <Button color="primary" id="save-entity" data-cy="entityCreateSaveButton" type="submit" disabled={updating}>
+                  <FontAwesomeIcon icon="save" />
+                  &nbsp;
+                  <Translate contentKey="entity.action.save">Save</Translate>
+                </Button>
+              </ValidatedForm>
+            )}
+          </Col>
+        </Row>
       </Modal>
     </div>
   );
