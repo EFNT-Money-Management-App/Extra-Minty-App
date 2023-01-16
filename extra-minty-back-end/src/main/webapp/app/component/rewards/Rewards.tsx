6fc7bb1191f666e5af948d19615758fb4097bd8d
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-01-16 17:17:43 -0500

    fixed and updated wording for features page

--- extra-minty-back-end/src/main/webapp/app/component/rewards/Rewards.tsx
+++ extra-minty-back-end/src/main/webapp/app/component/rewards/Rewards.tsx
@@ -5,9 +5,9 @@ const Rewards = () => {
     return(
         <div className="rewards-text">
             <section>Get rewarded for contributing towards your savings goal!</section>
-            <section>For each 
-                <span className="peppermint-points">$1.00</span> contributed to a savings account, 
-                <span className="peppermint-points">1</span> PepperMint Point earned. </section>
+            <section>For each  
+                <span className="peppermint-points"> $1.00</span> contributed to a savings account, 
+                <span className="peppermint-points"> 1</span> PepperMint Point earned. </section>
             <section>Earn badges for each PepperMint Point milestone you reach! </section>
         </div>
     )
